c574f2042b03effec7ea890349f2a2b039aad6df
Henrique Griepp <henriquegriepp@DESKTOP-SU6BTHJ>
2024-03-10 12:09:06 -0300

    Feat(navigation): Addition of the navigation menu opening feature in the mobile menu.
    Feat(language): Addition of the language icon triggering feature in the mobile menu.

--- src/assets/img/menu.tsx
+++ src/assets/img/menu.tsx
@@ -3,10 +3,12 @@
 function IconMenu() {
   return (
     <svg
+    className="size-full"
       xmlns="http://www.w3.org/2000/svg"
       width="24"
       height="24"
       viewBox="0 -960 960 960"
+      fill="currentColor"
     >
       <path d="M120-240v-80h720v80H120zm0-200v-80h720v80H120zm0-200v-80h720v80H120z"></path>
     </svg>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-import { Switch } from "../ui/switch";
-
+import { Switch as SwitchLang } from "../ui/switch";
 import dataLang from "../../assets/dataLang.ts";
 import { NavBarLanguage } from "../../assets/dataLang.ts";
 import { Link } from "react-router-dom";
 import IconClose from "@/assets/img/close.tsx";
-import { ModeToggle } from "../Theme/mode-toggle.tsx";
+import { ModeToggle as ThemeToogle } from "../Theme/mode-toggle.tsx";
+import IconMenu from "@/assets/img/menu.tsx";
 
 export function Header() {
   const [lang, setLang] = useState("pt-br");
   const [screen, setScreen] = useState(window.innerWidth);
   const [scroll, setScroll] = useState(false);
+  const [openNavBar, setOpenNavBar] = useState(false);
   const { navBar } = (dataLang[lang] as NavBarLanguage) || { navBar: [] };
 
   const handleLang = () => {
@@ -25,15 +26,15 @@ export function Header() {
   };
 
   const handleScroll = () => {
-    const scroll = window.scrollY >= 20 ? true : false
-    setScroll(scroll)
-  }
+    const scroll = window.scrollY >= 20 ? true : false;
+    setScroll(scroll);
+  };
 
-  console.log(scroll)
+  const handleToggleNavBar = () => setOpenNavBar((prevState) => !prevState);
 
   useEffect(() => {
     window.addEventListener("resize", handleScreenNavBar);
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
 
     // return () => {
     //   window.removeEventListener("resize", handleScreenNavBar);
@@ -43,26 +44,41 @@ export function Header() {
 
   const NavMob = () => {
     return (
-      <div className="bg-background w-full flex flex-col justify-between items-center p-1">
-        <div className="w-full flex flex-row justify-between items-center">
-          <Switch
+      <div
+        className={`${
+          openNavBar === false ? "" : "bg-background h-screen"
+        }  w-full   p-1`}
+      >
+        <div className="w-full grid grid-cols-3 place-items-center">
+          <div>
+          { openNavBar === false ? null : <SwitchLang
             checked={lang === "en"}
-            className="flex items-center"
+            className="justify-self-start"
             onClick={handleLang}
-          />
+          />}
+          </div>
           <span className="text-3xl items-center">MYAPP</span>
-          <button className="size-9">
-            <IconClose />
+          <button
+            className="size-9 justify-self-end"
+            onClick={handleToggleNavBar}
+          >
+           {openNavBar === false ? <IconMenu /> : <IconClose />} 
           </button>
+          <div className=" w-full col-span-3 ">
+            {openNavBar === false ? null : (
+              <ul className="h-full w-full flex flex-col items-center max-w-3xl text-2xl gap-3 p-5 ">
+                {navBar.map((item: { url: string; menu: string }) => (
+                  <li className="p-2" key={item.menu}>
+                    <Link to={item.url}>{item.menu}</Link>
+                  </li>
+                ))}
+                <li>
+                  <ThemeToogle />
+                </li>
+              </ul>
+            )}
+          </div>
         </div>
-        <ul className=" h-full flex flex-col justify-start items-center max-w-3xl text-2xl gap-3 p-5 ">
-          {navBar.map((item: { url: string; menu: string }) => (
-            <li className="p-2" key={item.menu}>
-              <Link to={item.url}>{item.menu}</Link>
-            </li>
-          ))}
-        </ul>
-        <ModeToggle />
       </div>
     );
   };
@@ -78,22 +94,25 @@ export function Header() {
             </li>
           ))}
         </ul>
-        <Switch
+        <SwitchLang
           checked={lang === "en"}
           className="flex items-center"
           onClick={handleLang}
         />
 
-<ModeToggle />
+        <ThemeToogle />
       </div>
     );
   };
 
   return (
     <>
-      <header className={`w-full fixed delay-200 ${scroll === true ? "bg-background" : ""} duration-1000`}>
+      <header
+        className={`w-full fixed delay-200 duration-700 ${
+          scroll === true ? `bg-background` : ``
+        }`}
+      >
         {screen >= 768 ? <NavWeb /> : <NavMob />}
-        
       </header>
     </>
   );
